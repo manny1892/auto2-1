3aa81a2701cb861f6b9e6d05209f21ef153d7557
manny1892 <manny1892@gmail.com>
2022-07-20 12:04:33 +0300

    CI dont work

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -20,7 +20,7 @@ class MobileBankApiTestV1 {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
         ;
     }
 }
